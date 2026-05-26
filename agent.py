021b0dab3295747bbd7fa9d6d14e4f6a1b914185
rivkamos <77576647+rivkamos@users.noreply.github.com>
2026-05-26 12:09:37 +0300

    fix model

--- agent.py
+++ agent.py
@@ -5,7 +5,7 @@ from tools import get_meetings
 
 # LLM (Gemini)
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.5-flash",
     google_api_key=os.getenv("GEMINI_API_KEY")
 )
 
